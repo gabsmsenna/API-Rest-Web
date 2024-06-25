f76ce2d4329f20e25b0e0d2b8fef179c87e447b1
Gabriel Senna <gabrielmagalhaes3344@gmail.com>
2024-06-25 18:03:23 -0300

    First commit

---
+++ src/main/java/com/gabrielsenna/API_Rest_Web/ApiRestWebApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielsenna.API_Rest_Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestWebApplication.class, args);
+	}
+
+}
