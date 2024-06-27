7c215c4c875e08255c6cf069290595102e5a60fa
Gabriel Senna <gabrielmagalhaes3344@gmail.com>
2024-06-27 15:47:49 -0300

    Create User Service and CRUD operations

---
+++ src/main/java/com/gabrielsenna/API_Rest_Web/services/UserService.java
@@ -0,0 +1,52 @@
+package com.gabrielsenna.API_Rest_Web.services;
+
+import com.gabrielsenna.API_Rest_Web.models.User;
+import com.gabrielsenna.API_Rest_Web.repositories.TaskRepository;
+import com.gabrielsenna.API_Rest_Web.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.management.RuntimeErrorException;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public User findById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException("User not found | ID: " + id));
+    }
+
+    @Transactional //Utilizar esta notação sempre que for realizar um insert ou update, para garantir a ATOMICIDADE
+    public User createUser(User obj) {
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        this.taskRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    @Transactional
+    public User updateUser(User obj) {
+        User newObject = findById(obj.getId());
+        newObject.setPassword(obj.getPassword());
+        return this.userRepository.save(newObject);
+    }
+
+    public void deleteUser(Long id) {
+        this.userRepository.findById(id);
+
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+           throw new RuntimeException("Não foi possível excluir o usuário, há entidades relacionadas! " + id);
+        }
+    }
+}
